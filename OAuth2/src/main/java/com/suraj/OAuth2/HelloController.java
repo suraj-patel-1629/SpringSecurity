7f4717be24c3163ddd13170aaaa0f9f1b94ceeed
suraj patel <sp9589890205@gmail.com>
2025-03-20 19:09:37 +0530

    [Suraj] add. Hands-on concept related to OAuth2

---
+++ OAuth2/src/main/java/com/suraj/OAuth2/HelloController.java
@@ -0,0 +1,13 @@
+package com.suraj.OAuth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String grret(){
+        return "Welcome to Telsucko";
+    }
+}
